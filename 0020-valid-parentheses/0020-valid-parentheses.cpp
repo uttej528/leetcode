9e1e3e00bc4610b3ac4272060ebd1c143b0a0a78
uttej528 <96988310+uttej528@users.noreply.github.com>
2024-08-01 18:12:51 +0530

    Time: 3 ms (41.12%), Space: 7.8 MB (35.78%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <string>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> temp;
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                temp.push(s[i]);
+            } else {
+                if(temp.empty()) {
+                    return false; // No matching opening bracket
+                }
+                char val = temp.top();
+                if((s[i] == ')' && val == '(') || 
+                   (s[i] == '}' && val == '{') || 
+                   (s[i] == ']' && val == '[')) {
+                    temp.pop();
+                } else {
+                    return false; // Mismatched opening bracket
+                }
+            }
+        }
+        
+        return temp.empty(); // Stack should be empty for valid string
+    }
+};
