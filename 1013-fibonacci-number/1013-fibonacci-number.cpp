77f9121d54a992c0a83acc9cebb572a3bb8c4135
uttej528 <96988310+uttej528@users.noreply.github.com>
2024-08-26 18:56:17 +0530

    Time: 2355 ms (16.01%), Space: 363.8 MB (10.14%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,33 @@
 class Solution {
+
+    int dynamic(int n,vector<int> &dp)
+    {
+        if(n<=1)
+        {
+            dp[n]=n;
+            return n;
+        }
+        if(dp[n]!=-1)
+        {
+          return dp[n];
+        }
+       return dp[n]=fib(n-1)+fib(n-2);
+
+    }
 public:
     int fib(int n) {
+
+/* 
+recurssive method
         if(n<=1)
         {
             return n;
         }
+       return fib(n-1)+fib(n-2);
+       */
 
-        return fib(n-1)+fib(n-2);
+      vector<int> dp(n+1,-1);
+      dynamic(n,dp);
+      return dp[n];
     }
 };
