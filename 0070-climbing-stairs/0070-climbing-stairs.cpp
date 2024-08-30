93ce379d69656458888a944cf2437eeea1ee28d6
uttej528 <96988310+uttej528@users.noreply.github.com>
2024-08-30 09:34:39 +0530

    Time: 3 ms (29.65%), Space: 7.7 MB (8.46%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+/*
+    int climbstair(int i,int n)
+    {
+        if(i==n)
+        {
+            return 1;
+        }
+        if(i>n)
+        {
+            return 0;
+        }
+
+        return climbstair(i+1,n)+climbstair(i+2,n);
+    }
+*/
+     int climbstair(int i,int n,vector<int>&dp)
+    {
+        if(i==n)
+        {
+            return 1;
+        }
+        if(i>n)
+        {
+            return 0;
+        }
+
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        return dp[i]=climbstair(i+1,n,dp)+climbstair(i+2,n,dp);
+    }
+    int climbStairs(int n) {
+
+        vector<int>dp(n+1,-1);
+     return climbstair(0,n,dp);
+     //int val=dp.size();
+     //return val;   
+    }
+};
