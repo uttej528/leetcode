2cc5c802a6d8a338e6c7010a32eb4b755d4137a9
uttej528 <96988310+uttej528@users.noreply.github.com>
2024-09-01 16:11:39 +0530

    Time: 2 ms (79.47%), Space: 12.6 MB (8%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        int i=0;int j=n;
+        int i=0;
         vector<int> temp;
         for(int i=0;i<n;i++)
         {
          temp.push_back(nums[i]);
          temp.push_back(nums[i+n]);
-         j++;
+        
         }
 
         return temp;
